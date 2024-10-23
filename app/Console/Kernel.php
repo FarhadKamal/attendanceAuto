31cd4c196321a1d03534ceb80dc0ec809d58aecd
Towhidul islam <39158631+towhidJ@users.noreply.github.com>
2024-10-23 09:58:47 +0600

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,17 +15,16 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         // $schedule->command('fetch:attendance-get')->everyMinute()
         // ->between('09:00', '19:00');
-        $schedule->command('fetch:attendence-new-155')->everyFifteenMinutes()
+        $schedule->command('fetch:attendence-new-155')->everyTenMinutes()
         ->between('09:00', '19:00');
-        $schedule->command('fetch:attendence-old-176')->everyTenMinutes()
+        $schedule->command('fetch:attendence-old-176')->everyFiveMinutes()
         ->between('09:00', '19:00');
         // $schedule->command('fetch:attendance-get')->cron('20 9 * * *'); // 9:20 AM
         // $schedule->command('fetch:attendance-get')->cron('20 10 * * *'); // 10:20 AM
         // $schedule->command('fetch:attendance-get')->cron('0 14 * * *');  // 2:00 PM
         // $schedule->command('fetch:attendance-get')->cron('0 16 * * *');  // 4:00 PM
         // $schedule->command('fetch:attendance-get')->cron('40 16 * * *');  // 4:00 PM
-        // $schedule->command('fetch:attendance-get')->cron('10 17 * * *');  // 4:00 PM
-        // $schedule->command('fetch:attendance-get')->cron('20 18 * * *'); // 6:20 PM
+
     }
 
     /**
